e2b4f74aecb70f40df8de7fbb66b14b214a4ba82
Jörg Reichert <de.abg.reichert.joerg@googlemail.com>
2021-09-26 11:53:30 +0200

    Unset picture when unhover, show alternative text when image file missing

--- docs/js/pictures.js
+++ docs/js/pictures.js
@@ -15,12 +15,12 @@ define(["jquery", "leaflet", "leaflet.ajax", "fancybox"], ($, leaflet, leafletAj
         htmlInner += '</div>'
         this._div.innerHTML = htmlInner;
         const id = state.getSelectedPump()
-        for (var i=0; i<3; i++) {
+        for (var i=0; i<5; i++) {
           $("#pumpsPhotoContainer a:last-child").remove();
         }
         if (id) {
           if ($("#pumpsPhotoContainer > img").length == 0) {
-            $("#pumpsPhotoContainer").append("<a data-fancybox='gallery' data-src='./images/" + id + "/01.jpg'><img src='./images/" + id + "/01.jpg' style='height:275px' /></a>");
+            $("#pumpsPhotoContainer").append("<a data-fancybox='gallery' data-src='./images/" + id + "/01.jpg'><img alt='Noch keine Fotos verfügbar' src='./images/" + id + "/01.jpg' style='height:275px' /></a>");
             $("#pumpsPhotoContainer").append("<a data-fancybox='gallery' data-src='./images/" + id + "/02.jpg'><img src='./images/" + id + "/02.jpg' style='width:0px' /></a>");
             $("#pumpsPhotoContainer").append("<a data-fancybox='gallery' data-src='./images/" + id + "/03.jpg'><img src='./images/" + id + "/03.jpg' style='width:0px' /></a>");
             $("#pumpsPhotoContainer").append("<a data-fancybox='gallery' data-src='./images/" + id + "/04.jpg'><img src='./images/" + id + "/04.jpg' style='width:0px' /></a>");
@@ -30,6 +30,11 @@ define(["jquery", "leaflet", "leaflet.ajax", "fancybox"], ($, leaflet, leafletAj
       };
       state.setPictures(picturePane);
       return picturePane;
+    },
+    resetPictures() {
+      for (var i=0; i<5; i++) {
+        $("#pumpsPhotoContainer a:last-child").remove();
+      }
     }
   };
 });
--- docs/js/show_pump_layer.js
+++ docs/js/show_pump_layer.js
@@ -1,4 +1,4 @@
-define(["jquery", "leaflet", "leaflet.ajax", "leaflet.markercluster", "progress", "map", "icon", "info"], ($, leaflet, leafletAjax, leafletClusterMap, updateProgressBar, map, icon, info) => {
+define(["jquery", "leaflet", "leaflet.ajax", "leaflet.markercluster", "progress", "map", "icon", "info", "pictures"], ($, leaflet, leafletAjax, leafletClusterMap, updateProgressBar, map, icon, info, pictures) => {
   return (state, data) => {
     state.setMatchCount(0);
     var clusterLayer = leaflet.markerClusterGroup({ chunkedLoading: true, chunkProgress: updateProgressBar, maxClusterRadius: () => 25 });
@@ -8,8 +8,8 @@ define(["jquery", "leaflet", "leaflet.ajax", "leaflet.markercluster", "progress"
     clusterLayer.addLayer(geoJsonLayer);
     state.getPumpMap().addLayer(clusterLayer);
     registerLayerMouseOver(state, icon, info, geoJsonLayer);
-    registerLayerMouseOut(state, icon, info, geoJsonLayer);
-    geoJsonLayer.on('click', registerLayerMouseClick(state, icon, info));
+    registerLayerMouseOut(state, icon, info, pictures, geoJsonLayer);
+    geoJsonLayer.on('click', registerLayerMouseClick(state, icon, info, pictures));
     state.getInfo().update();
     state.getPictures().update();
   }
@@ -25,28 +25,31 @@ function registerLayerMouseOver(state, icon, info, geoJsonLayer) {
   });
 };
 
-function registerLayerMouseOut(state, icon, info, geoJsonLayer) {
+function registerLayerMouseOut(state, icon, info, pictures, geoJsonLayer) {
   geoJsonLayer.on('mouseout', function (e) {
     if (!state.getLastCoordinates()) {
       info.resetHighlight(state, e);
       icon.resetIcon(state);
+      pictures.resetPictures();
     }
   });
 };
 
-function registerLayerMouseClick(state, icon, info) {
+function registerLayerMouseClick(state, icon, info, pictures) {
   return (e) => {
     var coordinates = e.layer.feature.geometry.coordinates;
     var layer = e.layer;
     if (state.getLastCoordinates() != coordinates) {
       if (state.getLastCoordinates()) {
         icon.resetIcon(state);
+        pictures.resetPictures();
         icon.changeIcon(state, e);
       }
       info.highlightFeature(state, e);
       state.setLastCoordinates(coordinates);
     } else {
       icon.resetIcon(state);
+      pictures.resetPictures();
       state.setLastCoordinates(undefined);
       state.setSelectedPump(undefined);
     }
