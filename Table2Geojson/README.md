3c9db3c5f57401b353279ee3defb2c89ec8f429a
Jörg Reichert <de.abg.reichert.joerg@googlemail.com>
2022-08-13 21:20:25 +0200

    Update gradle

--- Table2Geojson/README.md
+++ Table2Geojson/README.md
@@ -2,3 +2,6 @@
 ## Verlinkung
 * pumpen.csv WikipediaId = pumpen_wikipedia.json ID 
 * pumpen_wikipedia.json ID -> pumpen_osm.geojson ref:lfd 
+
+## Update Gradle
+* [Gradle Wrapper](https://docs.gradle.org/current/userguide/gradle_wrapper.html#sec:upgrading_wrapper): `./gradlew wrapper --gradle-version 7.5.1  --distribution-type bin --gradle-distribution-sha256-sum f6b8596b10cce501591e92f229816aa4046424f3b24d771751b06779d58c8ec4`
--- Table2Geojson/build.gradle.kts
+++ Table2Geojson/build.gradle.kts
@@ -3,7 +3,7 @@ version = "0.1-SNAPSHOT"
 
 plugins {
     application
-    kotlin("jvm") version "1.6.0"
+    kotlin("jvm") version "1.7.10"
 }
 
 java {
@@ -17,18 +17,18 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.10")
-    implementation("com.fasterxml.jackson.core:jackson-core:2.13.0")
-    implementation("com.fasterxml.jackson.core:jackson-annotations:2.13.0")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.13.0")
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-csv:2.13.0")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.0")
-	testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    implementation("com.fasterxml.jackson.core:jackson-core:2.13.3")
+    implementation("com.fasterxml.jackson.core:jackson-annotations:2.13.3")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.13.3")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-csv:2.13.3")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.3")
+	testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.6.0")
 }
 
 application {
-    mainClass.set("de.l.oklab.pumps.Table2GeojsonMain")
+    mainClass.set("de.l.oklab.pumps.Table2GeojsonMainKt")
 }
 
 
